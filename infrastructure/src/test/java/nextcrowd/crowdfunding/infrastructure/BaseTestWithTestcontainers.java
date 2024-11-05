5ca363d1f27820b208a6dc7e99dc324301528dc4
Antonio D'Alessio <antessio7@gmail.com>
2024-11-05 09:25:06 +0100

    configure test environment (#6)
    * configure test environment

--- infrastructure/src/test/java/nextcrowd/crowdfunding/infrastructure/BaseTestWithTestcontainers.java
+++ infrastructure/src/test/java/nextcrowd/crowdfunding/infrastructure/BaseTestWithTestcontainers.java
@@ -1,15 +1,9 @@
 package nextcrowd.crowdfunding.infrastructure;
 
-import javax.validation.constraints.NotNull;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
@@ -18,28 +12,28 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public abstract class BaseTestWithTestcontainers {
 
-    @Container
-    //@ServiceConnection
-    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.4")
-            .withDatabaseName("nextcrowd_db")
-            .withUsername("nextcrowd_root")
-            .withPassword("nextcrowd_pwd")
-            .withEnv("POSTGRES_HOST_AUTH_METHOD", "trust")
-            .withExposedPorts(5432)
-            .waitingFor(
-                    Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 1)
-            )
-            .withNetworkMode("bridge")
-            .withInitScript("init.sql");
+    private static PostgreSQLContainer<?> postgreSQLContainer;
 
-    /*@DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        *//*registry.add("spring.liquibase.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.liquibase.user", postgreSQLContainer::getUsername);
-        registry.add("spring.liquibase.password", postgreSQLContainer::getPassword);*//*
-    }*/
+    static {
+        if ("true".equals(System.getenv("ci"))) {
+            postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.4")
+                    .withDatabaseName("nextcrowd_db")
+                    .withUsername("nextcrowd_root")
+                    .withPassword("nextcrowd_pwd")
+                    .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 1))
+                    .withInitScript("init.sql");
+            postgreSQLContainer.start();
+        }
+    }
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        if (postgreSQLContainer != null) {
+            registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+            registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+            registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        }
+    }
+
 
 }
